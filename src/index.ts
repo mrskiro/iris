2e517de99ad3f4c80488e5526afbe55b4fcf79a5
mrskiro <mrskhrk@gmail.com>
2024-03-16 23:19:33 +0900

    t

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const date = (() => {
 })();
 
 (async () => {
-	console.log("EMAIL", process.env.EMAIL);
+	console.log("EMAIL", process.env.EMAIL?.length ?? 0);
 	if (
 		!process.env.EMAIL ||
 		!process.env.PASSWORD ||
